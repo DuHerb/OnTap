cfebf030f8dde8bbcf5f04748433f81f9b76c8b7
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-07-07 13:17:56 -0700

    add working links to views

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,6 +5,10 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+// import { Link } from '@material-ui/core';
+import {Link} from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,18 +20,41 @@ const useStyles = makeStyles(theme => ({
   title: {
     flexGrow: 1,
   },
+  navLink: {
+    textDecoration: "none"
+  }
 }));
 
 export default function Header(props) {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  function handleClick(event) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
 
   return (
     <div className={classes.root}>
       <AppBar position="static" color="white">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
             <MenuIcon />
           </IconButton>
+          <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}><Link className={classes.navLink} to="/">PatronView</Link></MenuItem>
+        <MenuItem onClick={handleClose}><Link className={classes.navLink} to="/pos">POSView</Link></MenuItem>
+        <MenuItem onClick={handleClose}><Link className={classes.navLink} to="/admin">AdminView</Link></MenuItem>
+      </Menu>
           <Typography variant="h6" className={classes.title}>
             On Tap
           </Typography>
