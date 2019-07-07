68da33521c5bfffde877c941759c5af1e66bba46
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-07-07 13:17:41 -0700

    remove react-router switch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './Components/Header';
 import PatronView from './Components/PatronView';
 import POSView from './Components/POSView';
 import AdminView from './Components/AdminView';
-import { Switch, Route, HashRouter } from 'react-router-dom';
+import { Route, HashRouter } from 'react-router-dom';
 
 export default class App extends React.Component {
   render() {
@@ -12,9 +12,9 @@ export default class App extends React.Component {
       <>
         <HashRouter>
           <Header/>
-            <Route exact path='/' render={()=><PatronView/>} />
-            <Route path='/pos' render={()=><POSView/>}  />
-            <Route path='/admin' render={()=><AdminView/>} />
+          <Route exact path='/' render={()=><PatronView/>} />
+          <Route path='/pos' render={()=><POSView/>}  />
+          <Route path='/admin' render={()=><AdminView/>} />
         </HashRouter>
       </>
     )
