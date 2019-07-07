020eb6eb4bed0e455b4cdcf3d7007cf4d180ed8a
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-07-07 16:55:26 -0700

    add proptypes

--- src/Components/PVCard.jsx
+++ src/Components/PVCard.jsx
@@ -4,13 +4,20 @@ import PropTypes from 'prop-types'
 function PVCard(props) {
   return (
     <div>
-      <p>{props.name}</p> 
+      <p>{props.name}</p>
     </div>
   )
 }
 
 PVCard.propTypes = {
-
+  name: PropTypes.string,
+  brewery: PropTypes.string,
+  style: PropTypes.string,
+  abv: PropTypes.string,
+  pintsLeft: PropTypes.string,
+  price: PropTypes.string,
+  description: PropTypes.string,
+  key: PropTypes.string,
 }
 
 export default PVCard
