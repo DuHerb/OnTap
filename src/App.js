7812d42f407e6ea03140a0543e535ebb99029c8c
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-07-12 12:54:24 -0700

    add and test handleSellPint

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import POSView from './Components/POSView';
 import AdminView from './Components/AdminView';
 import { Route, HashRouter } from 'react-router-dom';
 import { kegs } from './store.js'
+import { Button } from '@material-ui/core'
 
 export default class App extends React.Component {
   state = {
@@ -16,14 +17,29 @@ export default class App extends React.Component {
     return this.state.kegs.filter(keg => keg.onTap);
   }
 
+  getBeerIndex = (beerName) => kegs.findIndex((beer)=> beer.name === beerName)
+
+  handleSellPint = (beerName) => {
+    let selectedBeerIndex = this.getBeerIndex(beerName);
+    // console.log(selectedBeerIndex);
+    // console.log({...this.state.kegs[selectedBeerIndex]});
+    // let thisPintsLeft = this.state.kegs[selectedBeerIndex].pintsLeft;
+    // console.log(thisPintsLeft);
+    let updatedKegList = [...kegs];
+    updatedKegList[selectedBeerIndex].pintsLeft = updatedKegList[selectedBeerIndex].pintsLeft - 1;
+    this.setState({kegs: updatedKegList});
+  }
+
+
+
   render() {
-    // const kegsOnTap = this.getKegsOnTap();
 
     return (
         <HashRouter>
           <Header/>
-          <Route exact path='/' render={()=><PatronView kegs={this.getKegsOnTap()} />} />
-          <Route path='/pos' render={()=><POSView kegs={this.getKegsOnTap()} />}  />
+          <Button onClick={()=>this.handleSellPint('Twilight')}>Sell Pint</Button>
+          <Route exact path='/' render={()=><PatronView kegs={this.getKegsOnTap()}/>} />
+          <Route path='/pos' render={()=><POSView kegs={this.getKegsOnTap()} onSellPint={this.handleSellPint} />}  />
           <Route path='/admin' render={()=><AdminView/>} />
         </HashRouter>
     )
