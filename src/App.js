cb907fa91a615439d5cbb7d3cd25c41f20d851ef
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-07-14 13:15:00 -0700

    fix bug where updatedKegs was getting set to store Kegs instead of state...fixed deletion errors

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import POSView from './Components/POSView';
 import AdminView from './Components/AdminView';
 import { Route, HashRouter } from 'react-router-dom';
 import { kegs } from './store.js';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 export default class App extends React.Component {
   state = {
     kegs,
@@ -17,19 +17,19 @@ export default class App extends React.Component {
     return this.state.kegs.filter(keg => keg.onTap);
   }
 
-  getBeerIndex = (beerName) => kegs.findIndex((beer)=> beer.name === beerName)
+  getBeerIndex = (beerName) => this.state.kegs.findIndex((beer)=> beer.name === beerName)
 
   //Handler Functions
   handleSellPint = (beerName) => {
     let selectedBeerIndex = this.getBeerIndex(beerName);
-    let updatedKegs = [...kegs];
+    let updatedKegs = [...this.state.kegs];
     updatedKegs[selectedBeerIndex].pintsLeft = updatedKegs[selectedBeerIndex].pintsLeft - 1;
     this.setState({kegs: updatedKegs});
   }
 
   handleToggleTapStatus = (beerName) => {
     let selectedBeerIndex = this.getBeerIndex(beerName);
-    let updatedKegs = [...kegs];
+    let updatedKegs = [...this.state.kegs];
     updatedKegs[selectedBeerIndex].onTap = !updatedKegs[selectedBeerIndex].onTap;
     this.setState({kegs: updatedKegs});
     console.log(updatedKegs[selectedBeerIndex].onTap);
@@ -37,7 +37,7 @@ export default class App extends React.Component {
 
   handleRefillKeg = (beerName) => {
     let selectedBeerIndex = this.getBeerIndex(beerName);
-    let updatedKegs = [...kegs];
+    let updatedKegs = [...this.state.kegs];
     updatedKegs[selectedBeerIndex].pintsLeft = 124;
     this.setState({kegs: updatedKegs});
   }
@@ -45,7 +45,7 @@ export default class App extends React.Component {
   //Crud Actions
   handleDeleteKeg = (beerName) => {
     let selectedBeerIndex = this.getBeerIndex(beerName);
-    let updatedKegs = [...kegs];
+    let updatedKegs = [...this.state.kegs];
     updatedKegs.splice(selectedBeerIndex, 1);
     this.setState({kegs: updatedKegs});
   }
@@ -56,8 +56,8 @@ export default class App extends React.Component {
           <Header/>
             <Route exact path='/' render={()=><PatronView kegs={this.getKegsOnTap()}/>} />
             <Route path='/pos' render={()=><POSView kegs={this.getKegsOnTap()} onSellPint={this.handleSellPint}/>} />
-            <Route path='/admin' render={()=><AdminView kegsOnTap={this.getKegsOnTap()} kegs={this.state.kegs} onToggleTapStatus={this.handleToggleTapStatus} onHandleRefillKeg={this.handleRefillKeg}/>} />
-            <Button variant='outlined' onClick={()=>this.handleDeleteKeg('Mirror Pond')}>Test Delete Keg</Button>
+            <Route path='/admin' render={()=><AdminView kegsOnTap={this.getKegsOnTap()} kegs={this.state.kegs} onToggleTapStatus={this.handleToggleTapStatus} onRefillKeg={this.handleRefillKeg} onDeleteKeg={this.handleDeleteKeg}/>} />
+            {/* <Button variant='outlined' onClick={()=>this.handleDeleteKeg('Mirror Pond')}>Test Delete Keg</Button> */}
         </HashRouter>
     )
   }
