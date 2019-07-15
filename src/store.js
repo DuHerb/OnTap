d2440d39125569d62b0e59e2066eda9777270942
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-07-14 20:45:35 -0700

    add uid to store entries

--- src/store.js
+++ src/store.js
@@ -1,58 +1,62 @@
 export const kegs = [
-    {
-      name: 'Twilight',
-      brewery: 'Deschutes',
-      style: 'Pale Ale',
-      abv: '5.5',
-      pintsLeft: 124,
-      price: '6',
-      description: 'Cool, Light Bodied Summer Ale.',
-      onTap: true,
-      imageKey: 'twilight'
-    },
-    {
-      name: 'Mirror Pond',
-      brewery: 'Deschutes',
-      style: 'Pale Ale',
-      abv: '5',
-      pintsLeft: 124,
-      price: '5',
-      description: 'Classic, Style Defining Pale Ale',
-      onTap: true,
-      imageKey: 'mirrorpond'
-    },
-    {
-      name: 'Fresh Squeezed',
-      brewery: 'Deschutes',
-      style: 'IPA',
-      abv: '6.8',
-      pintsLeft: 124,
-      price: '6.5',
-      description: 'Citra and Mosiac Hops',
-      onTap: true,
-      imageKey: 'freshsqueezed'
-    },
-    {
-      name: 'Fresh Haze',
-      brewery: 'Deschutes',
-      style: 'Hazy IPA',
-      abv: '6.2',
-      pintsLeft: 124,
-      price: '6.5',
-      description: 'Citra and Mosiac Hops, but Hazy',
-      onTap: false,
-      imageKey: 'freshhaze'
-    },
-    {
-      name: 'Black Butte Porter',
-      brewery: 'Deschutes',
-      style: 'Porter',
-      abv: '5.2',
-      pintsLeft: 124,
-      price: '5.5',
-      description: 'Chocolate and Coffee Notes',
-      onTap: false,
-      imageKey: 'blackbutteporter'
-    }
-  ]
-
+  {
+    name: 'Twilight',
+    brewery: 'Deschutes',
+    style: 'Pale Ale',
+    abv: '5.5',
+    pintsLeft: 124,
+    price: '6',
+    description: 'Cool, Light Bodied Summer Ale.',
+    onTap: true,
+    imageKey: 'twilight',
+    uid: '1'
+  },
+  {
+    name: 'Mirror Pond',
+    brewery: 'Deschutes',
+    style: 'Pale Ale',
+    abv: '5',
+    pintsLeft: 124,
+    price: '5',
+    description: 'Classic, Style Defining Pale Ale',
+    onTap: true,
+    imageKey: 'mirrorpond',
+    uid: '2'
+  },
+  {
+    name: 'Fresh Squeezed',
+    brewery: 'Deschutes',
+    style: 'IPA',
+    abv: '6.8',
+    pintsLeft: 124,
+    price: '6.5',
+    description: 'Citra and Mosiac Hops',
+    onTap: true,
+    imageKey: 'freshsqueezed',
+    uid: '3'
+  },
+  {
+    name: 'Fresh Haze',
+    brewery: 'Deschutes',
+    style: 'Hazy IPA',
+    abv: '6.2',
+    pintsLeft: 124,
+    price: '6.5',
+    description: 'Citra and Mosiac Hops, but Hazy',
+    onTap: false,
+    imageKey: 'freshhaze',
+    uid: '4'
+  },
+  {
+    name: 'Black Butte Porter',
+    brewery: 'Deschutes',
+    style: 'Porter',
+    abv: '5.2',
+    pintsLeft: 124,
+    price: '5.5',
+    description: 'Chocolate and Coffee Notes',
+    onTap: false,
+    imageKey: 'blackbutteporter',
+    uid: '5'
+  }
+]
