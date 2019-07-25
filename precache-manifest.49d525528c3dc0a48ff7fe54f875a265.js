8c48b96ef99eede49ae4fcbbf3d008c39d7e5947
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-07-25 08:51:24 -0700

    Updates

---
+++ precache-manifest.49d525528c3dc0a48ff7fe54f875a265.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e96afeb45e2d0287f35f1ca947875b",
+    "url": "/OnTap/index.html"
+  },
+  {
+    "revision": "a71a906d6d381bd044b3",
+    "url": "/OnTap/static/css/main.b560beee.chunk.css"
+  },
+  {
+    "revision": "14bf0727517b66c76331",
+    "url": "/OnTap/static/js/2.9869e9c5.chunk.js"
+  },
+  {
+    "revision": "a71a906d6d381bd044b3",
+    "url": "/OnTap/static/js/main.b95357e3.chunk.js"
+  },
+  {
+    "revision": "a1362c5ee29f87224d91",
+    "url": "/OnTap/static/js/runtime~main.5db36a86.js"
+  },
+  {
+    "revision": "e475104b56b3497cac8334275bc86ff6",
+    "url": "/OnTap/static/media/blackbutteporter.e475104b.png"
+  },
+  {
+    "revision": "1c3a43e804ee90e5516c836367170669",
+    "url": "/OnTap/static/media/default.1c3a43e8.png"
+  },
+  {
+    "revision": "a74fa3c8e079069690c0fcdf9ad74e54",
+    "url": "/OnTap/static/media/deschuteslogo.a74fa3c8.png"
+  },
+  {
+    "revision": "0a33b0a0da8c25659062c4562fba96ab",
+    "url": "/OnTap/static/media/freshhaze.0a33b0a0.png"
+  },
+  {
+    "revision": "a094f50c904bec1bc6c95eafd33b0037",
+    "url": "/OnTap/static/media/freshsqueezed.a094f50c.png"
+  },
+  {
+    "revision": "b978bf37e88b109db1497e4257a829ff",
+    "url": "/OnTap/static/media/mirrorpond.b978bf37.png"
+  },
+  {
+    "revision": "cc728e4777865480814114944df79340",
+    "url": "/OnTap/static/media/portlandlocation.cc728e47.png"
+  },
+  {
+    "revision": "31291e7a65055404abf602428480c9a8",
+    "url": "/OnTap/static/media/twilight.31291e7a.png"
+  }
+]);
